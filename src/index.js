5fe22e0bfb5a04b1c7079383e7e67f3cda9721d1
lutherBenny <luthersasi@gmail.com>
2024-06-30 19:31:02 +0530

    update files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,68 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./style.css"; // Import the CSS file
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import one from "./assets/images/dog1.jpg";
+import two from "./assets/images/dog2.jpg";
+import three from "./assets/images/dog3.jpg";
+import four from "./assets/images/dog4.jpg";
+import five from "./assets/images/dog5.jpg";
+import six from "./assets/images/dog6.jpg";
+import seven from "./assets/images/dog7.jpg";
+import eight from "./assets/images/dog8.jpg";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function Dog(props) {
+    return (
+        <div className="card">
+            <img style={{width:"200px", height:"250px"}} src={props.image} alt="dog" />
+            <p>{props.description}</p>
+        </div>
+    );
+}
+
+// Create a list of dogs
+const dogList = [
+    {
+        image: one,
+        description: "Deep Sleeping"
+    },
+    {
+        image: two,
+        description: "Looking"
+    },
+    {
+        image: three,
+        description: "Big Eye"
+    },
+    {
+        image: four,
+        description: "Sleeping"
+    },
+    {
+        image: five,
+        description: "Wake Up See Sunlight"
+    },
+    {
+        image: six,
+        description: "Hands Shake"
+    },
+    {
+        image: seven,
+        description: "Travelling"
+    },
+    {
+        image: eight,
+        description: "Running"
+    }
+];
+
+// Render the dog list
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <div className="container">
+        {dogList.map((dog, index) => (
+            <Dog key={index} image={dog.image} description={dog.description} />
+        ))}
+    </div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
